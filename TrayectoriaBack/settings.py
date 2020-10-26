13060359b2c0b1c76dd636374c8c1fb1acdcf76b
roberto14118927 <robertoruiz141189@gmail.com>
2020-10-26 12:12:00 -0600

    add python decouple

--- TrayectoriaBack/settings.py
+++ TrayectoriaBack/settings.py
@@ -11,19 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config('SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^r=o&58_*rd24oh#ivny-k&7x(^6!ydr=o^oi+x3%4&c=l*v1$'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['web-ids.herokuapp.com']
 
@@ -100,27 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TrayectoriaBack.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':'trayectoriadb',
-#         'USER':'postgres',
-#         'PASSWORD': 'roberto141189',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432' 
-#     }
-# }
 
 DATABASES = {
     'default': {
